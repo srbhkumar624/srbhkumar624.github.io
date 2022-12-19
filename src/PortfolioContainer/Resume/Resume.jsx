3d9fab1bdfea85ec2573c138f6d54bbc04749d53
SaurabhKumar <srbhkumar624@gmail.com>
2022-12-19 17:58:43 +0530

    rectified the responsiveness

--- src/PortfolioContainer/Resume/Resume.jsx
+++ src/PortfolioContainer/Resume/Resume.jsx
@@ -62,42 +62,6 @@ export default function Resume() {
 
                     </div>
                 </div>
-                {/* <div><FcGraduationCap className='educationQualIcon' />
-                    <span className='educationQualText'>Educational Qualification</span>
-                    <div className="eduDiv">
-                        <div className='present'>
-                            <p className="line"></p>
-                            <p>June,2021 - Present</p>
-                            <p className="courseDate"></p>
-                            <div>
-                                <h2>Full Stack Developer</h2>
-                                <h6>Masai School, Banglore</h6>
-                                <p>Learning Full Stack Developement and Communication skills.</p>
-                            </div>
-                        </div>
-                        <div className='present'>
-                            <p className="line"></p>
-                            <p>2016 - 2020</p>
-                            <p className="courseDate"></p>
-                            <div>
-                                <h2>Computer Science Engineering</h2>
-                                <h6>MITS Rayagada, Odisha</h6>
-                                <p>Got 76.30% through out semester.</p>
-                            </div>
-                        </div>
-                        <div className='present'>
-                            <p className="line"></p>
-                            <p>2014 - 2016</p>
-                            <p className="courseDate"></p>
-                            <div>
-                                <h2>Intermediate</h2>
-                                <h6>PN College, Parsa</h6>
-                                <p>Got 66.40%.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div></div>
-                </div> */}
             </div>
             <div>
             </div>
