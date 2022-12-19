43b144289072e12d77bef9eb3bed5d4c8f673210
SaurabhKumar <srbhkumar624@gmail.com>
2022-12-19 19:22:03 +0530

    added stats

--- src/PortfolioContainer/Statistics/Stats.jsx
+++ src/PortfolioContainer/Statistics/Stats.jsx
@@ -6,11 +6,20 @@ import stylestats from '../Statistics/stats.module.css'
 const Stats = () => {
   return (
     <div className={stylestats.main}>
+      <div className={stylestats.main}>
       <h1 className={stylestats.heading}>Statistic</h1>
       <br />
-            <div style={{width:"100%",margin:"auto"}}>
-            
-            <img className={stylestats.img_div} src="https://i.ibb.co/V9tT9dk/Screenshot-203.png" alt="githubcalender" />
+        <img className={stylestats.image} src="https://github-readme-stats.vercel.app/api?username=srbhkumar624&show_icons=true&locale=en" alt="" />
+            <br />
+            <br />
+            <img className={stylestats.image} src="https://github-readme-streak-stats.herokuapp.com/?user=srbhkumar624&" alt="" />
+            <br />  
+            <h1 className={stylestats.heading}>Github Calender</h1>
+            <br />
+            <div style={{width:"80%",margin:"auto"}}>
+              {/* <Calender/> */}
+            <img className={stylestats.img_div} style={{width:"100%"}} src="https://i.ibb.co/SQgxqSS/Screenshot-203.png" alt="" />
+            </div>
             </div>
         
     </div>
