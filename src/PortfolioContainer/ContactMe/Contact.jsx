88f11a078d4a7d1769452862772e786c67066bd2
SaurabhKumar <srbhkumar624@gmail.com>
2022-12-19 16:38:16 +0530

    fixed email service

--- src/PortfolioContainer/ContactMe/Contact.jsx
+++ src/PortfolioContainer/ContactMe/Contact.jsx
@@ -13,13 +13,14 @@ export default function Contact() {
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        emailjs.sendForm('service_w2twb1n', 'template_eqiqgev', formRef.current, 'LwijkWWk2NvnhPASq')
+        emailjs.sendForm('service_b2g2w61', 'template_j1i2pse', formRef.current, 'LwijkWWk2NvnhPASq')
             .then((result) => {
                 console.log(result.text);
                 setDone(true)
             }, (error) => {
                 console.log(error.text);
             });
+            
     }
     return (
         <div className='c' id="contact">
@@ -57,7 +58,7 @@ export default function Contact() {
                     </p>
                     <form ref={formRef} onSubmit={handleSubmit}>
                         <input type="text" placeholder="Name" name="user_name" />
-                        <input type="text" placeholder="Subject" name="user_subject" />
+                        {/* <input type="text" placeholder="Subject" name="user_subject" /> */}
                         <input type="text" placeholder="Email" name="user_email" />
                         <textarea name="message" placeholder="Message" rows="5"></textarea>
                         <button className="submitBtn">Submit</button>
